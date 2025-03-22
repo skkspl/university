e9e241c7fa1ef9c7ef60b35c6cd09fa3c72850de
skkspl <Kseniaya2111@gmail.com>
2025-03-22 13:07:02 +0300

    Revert "Add tables (#6)"
    This reverts commit 7c025f459907aa2896a3c79b8728e509eea817bc.

--- app/src/main/resources/db/003-add-paragraph-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE paragraph (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    book_id INT NOT NULL,
-    paragraph_number INT NOT NULL,
-    text TEXT,
-    FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
-);
