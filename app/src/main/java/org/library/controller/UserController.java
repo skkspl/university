1d4270dd8e1494f480301448382443580f6ad5ad
KaMo User <ksyshabecouse@gmail.com>
2025-05-10 13:51:59 +0300

    enrich user api

--- app/src/main/java/org/library/controller/UserController.java
+++ app/src/main/java/org/library/controller/UserController.java
@@ -6,6 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.library.dto.request.UserRequest;
 import org.library.dto.response.UserResponse;
 import org.library.service.UserService;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,12 +21,28 @@ public class UserController {
 
     @Operation(summary = "Создание пользователя")
     @PostMapping
-    public UserResponse create(@RequestBody @Valid UserRequest request){
+    public UserResponse create(@RequestBody @Valid UserRequest request) {
         return userService.createUser(request);
     }
 
+    @PutMapping(path = "/{id}")
+    public UserResponse update(@PathVariable Long id, @RequestBody @Valid UserRequest request) {
+        return userService.updateUser(id, request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
     @GetMapping(path = "/{email}")
-    public UserResponse getByEmail(@PathVariable String email){
+    public UserResponse getByEmail(@PathVariable String email) {
         return userService.getUserByEmail(email);
+    }
+
+    @GetMapping
+    public Page<UserResponse> getAllUsers(@ParameterObject @PageableDefault(sort = "email") Pageable pageable) {
+        return userService.getAllUsers(pageable);
     }
 }
